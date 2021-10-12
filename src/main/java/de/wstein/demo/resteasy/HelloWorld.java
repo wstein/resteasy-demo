2a43a44ca8fb8ca0833a555c1e04b22a936821e1
Werner Stein <werner.stein@google.com>
2021-10-12 19:53:13 +0200

    05 new helloname method

--- src/main/java/de/wstein/demo/resteasy/HelloWorld.java
+++ src/main/java/de/wstein/demo/resteasy/HelloWorld.java
@@ -3,12 +3,17 @@ package de.wstein.demo.resteasy;
 import javax.ws.rs.*;
 
 @Path("tutorial")
-public class HelloWorld
-{
+public class HelloWorld {
 
     @GET
     @Path("helloworld")
     public String helloworld() {
         return "Hello World!";
+    }
+
+    @GET
+    @Path("helloname/{name}")
+    public String hello(@PathParam("name") final String name) {
+        return "Hello " + name;
     }
 }
